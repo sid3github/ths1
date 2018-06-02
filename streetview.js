b6b5da39449355a9b8ec2d6e2b135348fa904744
sid3github <sid3github3>
2018-06-01 23:46:53 -0700

    jquery added

--- streetview.js
+++ streetview.js
@@ -1,15 +1,39 @@
-$('#submit-btn').on('click', function () {
 
-    // var body = $('body');
-    var street = $('#street').val();
-    var city = $('#city').val();
-    var address = street + ',' + city;
+$('document').ready(function () {
+    $('#submit-btn').on('click', function () {
+        // var body = $('body');
+        var street = $('#street').val();
+        var city = $('#city').val();
+        var address = street + ',' + city;
+        var googleMapsBaseUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
+        var googleMapsUrl = googleMapsBaseUrl + address + '&key=AIzaSyCpFT1rfG93z6U4AHowzjpT96WEqozjJRs';
+        var backgroundImage = '<img class="bgimg" src=" ' + googleMapsUrl + ' ">';
 
+        $('#map .bgimg').remove();
+        $('#map').append(backgroundImage);
 
-    var googleMapsBaseUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
-    var googleMapsUrl = googleMapsBaseUrl + address + '&key=AIzaSyCpFT1rfG93z6U4AHowzjpT96WEqozjJRs';
-    var backgroundImage = '<img class="bgimg" src=" '+ googleMapsUrl +' ">';
+//wiki_info
+    var wikiLocation = street + ',' + '_' + city;
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' + wikiLocation;
+    console.log(wikiURL);
 
-    $('#map .bgimg').remove();
-    $('#map').append(backgroundImage);
+    function printContent(result) {
+        var wikiContent = '<p class="wiki-content"></p>';
+        $('#content').append(wikiContent);
+        $('.wiki-content').html(result[2]);
+    }
+
+    $.ajax(wikiURL, {
+        dataType: 'jsonp',
+        method: 'GET',
+        success: function (result) {
+            console.log(result);
+            printContent(result);
+        },
+        error: function (data) {
+            console.log(data);
+        }
+    });
+
+    });
 });
